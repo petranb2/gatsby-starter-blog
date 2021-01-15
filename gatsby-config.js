8127d7309790e093533d060a18f97c00368dd785
Petros Koulianos <petranb2@gmail.com>
2021-01-15 21:40:02 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Petros Koulianos Dev Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Petros Koulianos`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `A dev blog with main interest web development`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
       twitter: `kylemathews`,
